34615e11c75a2d18e72184e7d64f13c9d791c956
Johnny Backlund <johnnybacklund@hotmail.com>
2021-12-14 22:27:39 +0100

    Struggling with date / time-picker

--- components/ReservationModal.js
+++ components/ReservationModal.js
@@ -1,12 +1,21 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useOutsideAlerter } from "../utils/Hooks";
-import TextField from "@mui/material/TextField";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function ReservationModal({
   setShowModal,
   setShowReservationModal,
 }) {
   const reservationRef = useRef(null);
+
+  const [name, setName] = useState("");
+  const [numberOfGuests, setNumberOfGuests] = useState("0");
+  const [date, setDate] = useState(new Date());
+
+  const handleChange = (event) => {
+    setNumberOfGuests(event.target.value);
+  };
 
   useOutsideAlerter(reservationRef, () => {
     setShowReservationModal(false);
@@ -24,11 +33,58 @@ export default function ReservationModal({
           Make a reservation
         </h1>
       </div>
-      <form className="my-5">
-        <div className="relative mt-10">
-          <input type="text" className="inputText" placeholder=" " required />
-          <span className="floating-label">Full name</span>
-          <TextField label="Full name" />
+      <form className="mt-10">
+        <div className="sm:flex">
+          <div className="relative w-full sm:w-1/2">
+            <input
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              type="text"
+              className="inputText w-full"
+              placeholder=" "
+              required
+            />
+            <span className="floating-label">Full name</span>
+          </div>
+          <div className="relative w-full sm:w-1/2 mt-6 sm:mt-0 sm:ml-2">
+            <input
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              type="text"
+              className="inputText w-full"
+              placeholder=" "
+              required
+            />
+            <span className="floating-label">Email</span>
+          </div>
+        </div>
+        <div className="relative w-full mt-7 flex">
+          <div className="w-1/2 relative">
+            <select
+              className={`inputText w-full`}
+              name="guests"
+              id="numberOfGuests"
+              onChange={(e) => {
+                setNumberOfGuests(e.target.value);
+              }}
+            >
+              <option value="0">Guests</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+            </select>
+            <span
+              className={`floating-label ${
+                numberOfGuests != "0" ? " left-1 -top-5" : "top-2 opacity-0"
+              }`}
+            >
+              Guests
+            </span>
+          </div>
         </div>
       </form>
     </div>
